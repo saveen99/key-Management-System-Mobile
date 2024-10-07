e0de68ddfc6367ae3906781bd04e13a3423a3f7d
Thevni <153171327+Thevni@users.noreply.github.com>
2024-10-07 12:18:20 +0530

    Update Dashboard

--- app/src/main/java/com/example/myapplication/Dashboard.kt
+++ app/src/main/java/com/example/myapplication/Dashboard.kt
@@ -1,20 +1,35 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Dashboard : AppCompatActivity() {
+
+    private lateinit var Logout: Button
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
+
+        Logout=findViewById(R.id.logout)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dashoard)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Logout.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
